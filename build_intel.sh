2d82113dbb9d295fd6b4d9ac716f519064f30d39
Anton Samarchyan <desecho@gmail.com>
2026-03-13 07:41:41 -0400

    Include build_intel.sh script

---
+++ build_intel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+APP_NAME="Rat"
+BUNDLE="${APP_NAME}.app"
+
+mkdir -p "${BUNDLE}/Contents/MacOS"
+mkdir -p "${BUNDLE}/Contents/Resources"
+cp Rat/Info.plist "${BUNDLE}/Contents/"
+cp -R Rat/Resources/. "${BUNDLE}/Contents/Resources/"
+
+swiftc \
+    -o "${BUNDLE}/Contents/MacOS/${APP_NAME}" \
+    -framework AppKit \
+    -framework QuartzCore \
+    -target x86_64-apple-macos14.0 \
+    $(find Rat -name "*.swift")
+
+echo "Built ${BUNDLE} (x86_64) successfully."
